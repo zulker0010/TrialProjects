14e4f354294cb038a32f31bf9f19decceda60959
Zulker Yusuf <zulkeryusuf@gmail.com>
2022-03-28 20:55:09 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,31 @@
+//document.getElementById("count-el").innerText=5//
+
+// let myAge=20+5;
+
+// console.log(myAge);
+
+// let firstbatch=6
+
+
+// let secondbatch=5
+
+// let count=firstbatch+secondbatch;
+
+// console.log(count);
+
+// let myAge=20;
+// let humandogratio=7
+// let mydogAge=myAge*humandogratio;
+// console.log(mydogAge);
+
+let bonuspoints=50
+console.log(bonuspoints)
+
+bonuspoints=bonuspoints+50
+console.log(bonuspoints)
+
+bonuspoints=bonuspoints-75
+console.log(bonuspoints)
+
+bonuspoints=bonuspoints+45
+console.log(bonuspoints)
